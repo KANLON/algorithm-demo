e427e846b44446dc588923f4d317c42530f6d3cb
KANLON <s19961234@126.com>
2019-02-06 22:37:21 +0800

    添加面试题33题：把数组排成最小的数的题目

---
+++ interviewOffer/src/com/kanlon/sort_array_for_min_number/SortArrayForMinNumber.java
@@ -0,0 +1,26 @@
+package com.kanlon.sort_array_for_min_number;
+
+/**
+ * 面试题33：把数组排成最小的数
+ * 题目：输入一个正整数数组，把数组里所有数字拼接起来排成一个数，打印能拼接出的所有数字中最小的一个。例如输入数组{3,32,321},则打印出这三个数字能排成的最小数字321323。
+ *
+ *
+ * @author zhangcanlong
+ * @date 2019年2月6日
+ */
+public class SortArrayForMinNumber {
+	public static void main(String[] args) {
+
+	}
+
+	/**
+	 * 解题思路1（暴力破解）：对数组进行全排序，然后比较每个元素的大小。n!
+	 *
+	 * @param numbers
+	 *            数组
+	 */
+	void printMinNumber1(int[] numbers) {
+
+	}
+
+}
