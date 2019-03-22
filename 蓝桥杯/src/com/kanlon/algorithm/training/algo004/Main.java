f3137073cfbee7af876d4865e8945f2fa78f1cf4
kanlon <s19961234@126.com>
2019-03-22 16:27:38 +0800

    暂时完成ALGO 004 算法训练 结点选择的答案，后面应该还需要修改

--- 蓝桥杯/src/com/kanlon/algorithm/training/algo004/Main.java
+++ 蓝桥杯/src/com/kanlon/algorithm/training/algo004/Main.java
@@ -15,43 +15,62 @@ import javax.swing.tree.TreeNode;
  * @version: V1.0
  */
 public class Main {
+	
+	//存储树的结构，用邻接表存储树
+	private static List<List<Integer>> v = new ArrayList<List<Integer>>();
+	//存储权值和
+	private static int[][] dp=new int[100005][2];;
+	//表示结点是否被访问，100005为最大结点数
+	private static boolean[] visit=  new boolean[100005];
+	
 	public static void main(String[] args) {
 		solution1();
 	}
 	
 	/**
-	 * 解题思路1：
+	 * 解题思路1:    参考：https://blog.csdn.net/richenyunqi/article/details/78834248
 	 */
 	public static void solution1(){
 		Scanner scan = new Scanner(System.in);
 		//表示树结点数
 		int n = scan.nextInt();
-		//表示结点对应的权重，即值
-		DefaultMutableTreeNode[] nodes = new DefaultMutableTreeNode[n];
-		for(int i=0;i<nodes.length;i++){
-			DefaultMutableTreeNode node = new DefaultMutableTreeNode(scan.nextInt());
-			nodes[i]=node;
+        
+		for(int i=1;i<=n;i++){
+			//将第一行的存放其本身节点的值
+			dp[i][0]=scan.nextInt();
+            v.add(new ArrayList<Integer>());
 		}
 		
 		//存储边的情况，并将边连接到对应结上
-		List<int[]> sideList = new ArrayList<int[]>();
 		for(int i=0;i<n-1;i++){
-			int[] temp = new int[2];
-			temp[0]=scan.nextInt();
-			temp[1]=scan.nextInt();
-			sideList.add(temp);
-			//连接节点
-			nodes[temp[0]-1].add(nodes[temp[1]-1]);
+			//用邻接表存储图的节点
+			int a = scan.nextInt()-1;
+			int b = scan.nextInt()-1;
+			v.get(a).add(b);
+			v.get(b).add(a);
 		}
-		
-		if(n==1){
-			System.out.println(nodes[0]);
-			return;
-		}
-		
-		//树的根节点
-		DefaultMutableTreeNode root = nodes[0];
-			
-		
+		dfs(0);
+		//深度遍历
+		System.out.println(Math.max(dp[0][0], dp[0][1]));
 	}
+	/**
+	 * 深度遍历
+	 * @param root 树的顶点在顶点数组中的位置
+	 */
+	private static void dfs(int root){
+		List<Integer> temp = v.get(root);
+		//已访问
+		visit[root]=true;
+		//遍历某个root节点的所有子节点
+		for(int i=0;i<temp.size();i++){
+			//如果子节点没有访问过
+			if(!visit[temp.get(i)])
+				//递归遍历
+				dfs(temp.get(i));
+				//更新选中这个结点的整颗子树的最大权值和
+				dp[root][0]+=dp[temp.get(i)][1];
+				//更新没有选中这个结点的整棵子树的最大权值和
+				dp[root][1]+=Math.max(dp[temp.get(i)][0],dp[temp.get(i)][1]);
+			}
+		}
 }
