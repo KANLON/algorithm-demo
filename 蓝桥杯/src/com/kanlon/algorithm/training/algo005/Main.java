1ea3c1922cadc147d4cdb724bbc59ea1626106fc
kanlon <s19961234@126.com>
2019-03-30 22:41:36 +0800

    添加ALGO5 算法训练 最短路 题目

---
+++ 蓝桥杯/src/com/kanlon/algorithm/training/algo005/Main.java
@@ -0,0 +1,12 @@
+package com.kanlon.algorithm.training.algo005;
+/**
+ * ALGO5 算法训练 最短路  
+ * @author zhangcanlong
+ * @since 2019-3-30 下午10:40:37
+ * @version: V1.0
+ */
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
